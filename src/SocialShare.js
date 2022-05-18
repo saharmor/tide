5e7bf9add73799fb5477916a2b9fb3a3592f7839
saharmor <saharhashai@gmail.com>
2022-05-17 21:17:51 -0700

    score logic changed

---
+++ src/SocialShare.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {FacebookShareButton, RedditShareButton, TwitterShareButton, TwitterIcon, FacebookIcon, RedditIcon} from "react-share";
+
+
+const SocialShare = ({ url, title }) => {
+    return (
+        <span>
+        <TwitterShareButton url={url} title={title}>
+          <TwitterIcon size={24} round />
+        </TwitterShareButton>
+        &nbsp;
+        <FacebookShareButton url={url} quote={title}>
+          <FacebookIcon size={24} round />
+        </FacebookShareButton>
+        &nbsp;
+        <RedditShareButton url={url} title={title}>
+          <RedditIcon size={24} round />
+        </RedditShareButton>
+        </span>
+    );
+
+}
+
+export default SocialShare;
