e259a4642ab30326b84ebcbca83936a7747a483e
saharmor <saharhashai@gmail.com>
2022-06-04 13:41:51 -0700

    Added average score

--- src/SocialShare.js
+++ src/SocialShare.js
@@ -1,19 +1,20 @@
 import React from "react";
 import {FacebookShareButton, RedditShareButton, TwitterShareButton, TwitterIcon, FacebookIcon, RedditIcon} from "react-share";
 
+import { trackEvent } from "./utils"
 
-const SocialShare = ({ url, title }) => {
+const SocialShare = ({ url, title,sessionId }) => {
     return (
         <span>
-        <TwitterShareButton url={url} title={title + " #dalle"}>
+        <TwitterShareButton url={url} title={title + " #dalle"} beforeOnClick={(() => trackEvent('social', 'share_twitter', sessionId))}>
           <TwitterIcon size={24} round />
         </TwitterShareButton>
         &nbsp;
-        <FacebookShareButton url={url} quote={title}>
+        <FacebookShareButton url={url} quote={title} beforeOnClick={(() => trackEvent('social', 'share_facebook', sessionId))}>
           <FacebookIcon size={24} round />
         </FacebookShareButton>
         &nbsp;
-        <RedditShareButton url={url} title={title}>
+        <RedditShareButton url={url} title={title} beforeOnClick={(() => trackEvent('social', 'share_reddit', sessionId))}>
           <RedditIcon size={24} round />
         </RedditShareButton>
         </span>
